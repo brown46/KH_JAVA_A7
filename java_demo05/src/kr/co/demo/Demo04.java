9c1234ef6f4b65a40e75ecd21efac4e3649d9c0b
brown46 <rlawlsxo623@naver.com>
2022-10-13 15:20:08 +0900

    동적 배열

---
+++ java_demo05/src/kr/co/demo/Demo04.java
@@ -0,0 +1,38 @@
+package kr.co.demo;
+
+import java.util.Scanner;
+
+public class Demo04 {
+
+	public static void main(String[] args) {
+		/*
+		 * 동적 배열
+		 */
+		
+		Scanner sc =new Scanner(System.in);
+		
+		int arr1[] =new int[3];
+		arr1[0]=1; arr1[1]=2 ;arr1[2]=3;
+		
+		int tmp[];
+		for(int i=0;i<5;i++) {
+			System.out.print("정수값을 입력하세요: ");
+			int num =sc.nextInt();
+			
+			tmp =new int[arr1.length+1];
+			System.arraycopy(arr1,0,tmp,0,arr1.length);
+			
+			tmp[tmp.length-1]=num;
+			arr1= tmp;
+			
+			System.out.printf("arr1 배열의 크기가 %d가 되었습니다.\n",arr1.length);
+			
+		}
+		for(int i=0; i<arr1.length;i++) {
+			System.out.printf("%d\t",arr1[i]);
+		}
+		
+
+	}
+
+}
---
+++ java_demo05/src/kr/co/demo/Demo05.java
@@ -0,0 +1,36 @@
+package kr.co.demo;
+
+import java.util.Arrays;
+
+
+public class Demo05 {
+
+	public static void main(String[] args) {
+		/*
+		 *동적 배열
+		 * Arrays.copyOf를 이용한 깊은 복사	 
+		 * 만약 동일한 크기의 새로운 배열을 복사하고 싶은 경우
+		 * 원본 배열.clone() 하면 됨 
+		 */
+		
+		int arr1[] =new int[3];
+		arr1[0]=1; arr1[1]=2 ;arr1[2]=3;
+		
+		arr1=Arrays.copyOf(arr1,arr1.length+3 ); //
+		              //원본   //길이
+		for(int i=3; i>0;i--) {
+			arr1[arr1.length-i]=4;
+		}
+		
+		
+
+		int arr2[]= arr1.clone();//복제. 기존 배열에 영향이 가지 않는다.(얕은복사가 아님)
+		
+		for(int i=0; i<arr1.length;i++) {
+			System.out.printf("%d\t",arr1[i]);
+		}
+
+
+	}
+
+}
