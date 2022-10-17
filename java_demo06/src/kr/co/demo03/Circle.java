36d13cf71e3172b0a4f2010014153f7250ed0dc1
brown46 <rlawlsxo623@naver.com>
2022-10-17 14:56:57 +0900

    Circle 클래스

--- java_demo06/src/kr/co/demo03/Circle.java
+++ java_demo06/src/kr/co/demo03/Circle.java
@@ -2,7 +2,10 @@ package kr.co.demo03;
 
 public class Circle {
 
-	//private final double PI= 3.14;
+	//멤버 변수/상수 : final 키워드가 사용되면 상수
+	private final double PI1=3.14;
+	
+	//클래스 변수/상수 : static키워드가 사용되면 클래스 공유자원
 	public static final double PI=3.14;
 	private double radius;
 	
