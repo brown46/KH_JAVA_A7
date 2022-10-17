e4b1071dc06957649c374a9d411c232ebac9b494
brown46 <rlawlsxo623@naver.com>
2022-10-17 15:02:41 +0900

    객체 연습문제

---
+++ java_demo06/src/kr/co/exam01/Exam01.java
@@ -0,0 +1,22 @@
+package kr.co.exam01;
+
+public class Exam01 {
+	public static void main(String[] args) {
+		/*
+		 * 펜(Pen) 객체를 클래스로 만들어 보시오
+		 */
+		Pen pen = new Pen();
+		pen.color="black";
+		pen.ballsize=.5;
+		pen.type="ink";
+		
+		/*
+		 * 신발 객체를 클래스로 만들어 보시오
+		 */
+		
+		Shoes shoes= new Shoes();
+		shoes.feetsize=270;
+		shoes.type="운동화";
+		shoes.color="white";
+	}
+}
