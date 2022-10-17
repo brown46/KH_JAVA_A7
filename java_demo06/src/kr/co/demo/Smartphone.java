7550a4c8c2671961cd5970be9ed2915c87dbd768
brown46 <rlawlsxo623@naver.com>
2022-10-17 15:00:55 +0900

    Smartphone 클래스

---
+++ java_demo06/src/kr/co/demo/Smartphone.java
@@ -0,0 +1,31 @@
+package kr.co.demo;
+
+public class Smartphone {
+
+	private String model;
+	private String brand;
+	private String number;
+	
+	public void setModel(String model) {
+		this.model= model;
+		
+	}
+	public void setBrand(String brand) {
+		this.brand= brand;
+		
+	}
+	public void setNumber(String number) {
+		this.number= number;
+		
+	}
+	public String getModel() {
+		return this.model;
+	}
+	public String getBrand() {
+		return this.brand;
+	}
+	public String getNumber() {
+		String num= this.number.substring(0,9);
+		return num+"xxxx";
+	}
+}
