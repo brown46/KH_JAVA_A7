2e4e1fc310011f1b43ee6b68b6699ef181369219
brown46 <rlawlsxo623@naver.com>
2022-10-18 15:04:58 +0900

    객체 연습문제

--- java_demo06/src/kr/co/exam03/Exam.java
+++ java_demo06/src/kr/co/exam03/Exam.java
@@ -13,9 +13,57 @@ public class Exam {
 		 * 
 		 */
 
-		Clock c= new Clock(3,9,52);
+//		Clock c= new Clock(3,9,52);
 		//System.out.println(c.getTimeString());
-		c.getTime();
+//		System.out.println(c.getTimeString());
+//		c.getTime();
+		
+//		Speaker s= new Speaker(0,true,true);
+//		Speaker s1= new Speaker(3,true,true);
+//		System.out.println(s.getState());
+//		s.toggleMute();
+//		System.out.println(s.getState());
+//		s.upVolumn();
+//		System.out.println(s.getState());
+//		s.togglePower();
+//		System.out.println(s.getState());
+//		System.out.println(s1.getState());
+//		s1.toggleMute();
+//		s1.downVolumn();
+//		System.out.println(s1.getState());
+		
+//		Elevator e= new Elevator(12);
+//		Elevator e1= new Elevator(-3,12);
+//		e.moveTar(10);
+//		e.moveTar(5);
+//		e.moveTar(13);
+//		e.moveTar(7);
+//		e.moveTar(0);
+//		e1.moveTar(0);
+//		e1.moveTar(-3);
+		
+		FoodMenu f= new FoodMenu();
+		f.addFood("제육", 5000);
+		f.addFood("돈까스", 8000);
+		f.addFood("라면", 4000);
+		f.addFood("얼큰순대국", 7000);
+		
+		f.getMenu();
+		System.out.println();
+		f.delFood("돈까스");
+	
+		f.getMenu();
+		System.out.println();
+		f.addFood("피자", 10000);
+		f.addFood("치킨", 16000);
+		f.addFood("햄버거", 8500);
+		f.getMenu();
+		f.delFood("김밥");
+		System.out.println();
+		f.getMenu();
+		
+		
+		
 	}
 
 }
