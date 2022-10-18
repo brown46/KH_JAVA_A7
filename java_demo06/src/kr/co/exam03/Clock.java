ee91623ee1f29d6b0d09583b2e960281dc681e42
brown46 <rlawlsxo623@naver.com>
2022-10-18 12:09:27 +0900

    Clock 클래스

--- java_demo06/src/kr/co/exam03/Clock.java
+++ java_demo06/src/kr/co/exam03/Clock.java
@@ -4,22 +4,34 @@ public class Clock {
 	Clock() {}
 
 	Clock(int hour, int minute, int second) {
-		this.hour = hour;
-		this.minute = minute;
-		this.second = second;
+//		this.hour = hour;
+//		this.minute = minute;
+//		this.second = second;
+		setHour(hour);
+		setMinute(minute);
+		this.setSecond(second);
 	}
 
 	private static final int defaultTime = 0;
 
-	int hour = defaultTime;
-	int minute = defaultTime;
-	int second = defaultTime;
+	private int hour = defaultTime;
+	private int minute = defaultTime;
+	private int second = defaultTime;
 
+	public void setHour(int hour) {
+		this.hour = hour%24;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute%60;
+	}
+	public void setSecond(int second) {
+		this.second = second%60;
+	}
 	public String getTimeString() {
 		return hour + ":" + minute + ":" + second;
 	}
 	public void getTime() {
-		System.out.printf("%02d:%02d:%02d",hour,minute,second);
+		System.out.printf("%02d:%02d:%02d\n",hour,minute,second);
 	}
-
+	
 }
