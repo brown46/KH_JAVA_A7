9eb4e1b7537ef8ca494d5e632096d72114891dbb
brown46 <rlawlsxo623@naver.com>
2022-10-19 15:12:54 +0900

    FoodMenu 클래스

--- java_demo06/src/kr/co/exam03/FoodMenu.java
+++ java_demo06/src/kr/co/exam03/FoodMenu.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 public class FoodMenu {
 
-//	private String foodN[] = new String[0];
-	private String foodN[] = {"김밥"};
-//	private int foodC[] =new int[0];
-	private int foodC[] = {4000};
+////	private String foodN[] = new String[0];
+//	private String foodN[] = {"김밥"};
+////	private int foodC[] =new int[0];
+//	private int foodC[] = {4000};
+	
+	private Food food[] =new Food[0];  //객체 배열
 
 	public void getMenu() {
-		for (int i = 0; i < foodN.length; i++) {
+		for (int i = 0; i < food.length; i++) {
 			//System.out.println(foodN[i] + ": " + foodC[i] + "원");
-			System.out.printf("%s\t%,d원\n",foodN[i],foodC[i]);
+			System.out.printf("%s\t%,d원\n",food[i].name,food[i].price);
 		}
 	}
+	
+	
 //	public String getMenu() {
 //		String res="";
 //		for(int i=0;i<foodN.length;i++) {
@@ -24,29 +28,27 @@ public class FoodMenu {
 //	}
 
 	public void findFood(String food) {
-		for (int i = 0; i < foodN.length; i++)
-			if (food.equals(foodN[i])) {
-				System.out.println(foodC[i] + "원");
+		for (int i = 0; i <this.food.length; i++)
+			if (food.equals(this.food[i].name)) {
+				System.out.println(this.food[i].price + "원");
 			}
 	}
 
 	public void addFood(String food, int cost) {
-		for (int i = 0; i < foodN.length; i++) {//중복입력 방지
-			if (food.equals(foodN[i])) {
+		for (int i = 0; i < this.food.length; i++) {//중복입력 방지
+			if (food.equals(this.food[i].name)) {
 				System.out.println("중복입력");
 				return;
 			}
 		}
-		foodN = Arrays.copyOf(foodN, foodN.length + 1);
-		foodN[foodN.length - 1] = food;
-		foodC = Arrays.copyOf(foodC, foodC.length + 1);
-		foodC[foodC.length - 1] = cost;
+		this.food = Arrays.copyOf(this.food, this.food.length + 1);
+		this.food[this.food.length - 1] =new Food(food,cost);
 	}
 
 	public void modFood(String food, int cost) {
-		for (int i = 0; i < foodN.length; i++)
-			if (food.equals(foodN[i])) {
-				foodC[i] = cost;
+		for (int i = 0; i < this.food.length; i++)
+			if (food.equals(this.food[i].name)) {
+				this.food[i].price = cost;
 			}
 	}
 
@@ -58,17 +60,51 @@ public class FoodMenu {
 //			}
 //	}
 	public void delFood(String food) {
-		for (int i = 0; i < foodN.length; i++) {
-			if (food.equals(foodN[i])) {
-				for (int j = i; j < foodN.length - 1; j++) {
-					foodN[j] = foodN[j + 1];
-					foodC[j] = foodC[j + 1];
+		for (int i = 0; i < this.food.length; i++) {
+			if (food.equals(this.food[i].name)) {
+				for (int j = i; j < this.food.length - 1; j++) {
+					this.food[j].name = this.food[j + 1].name;
+					this.food[j].price = this.food[j+1].price;
 				}
-				foodN = Arrays.copyOf(foodN, foodN.length - 1);
-				foodC = Arrays.copyOf(foodC, foodC.length - 1);
+				this.food = Arrays.copyOf(this.food, this.food.length - 1);
+
 				break;
 			}
 		}
 	}
-
+	
+	private int findIndex(String name) {
+		// 메뉴를 찾아서 찾은 메뉴의 위치(Index)를 반환하기 위한 메서드
+		int idx = -1;
+		for(int i = 0; i < this.food.length; i++) {
+			if(this.food[i].equals(name)) {
+				idx = i;
+				break;
+			}
+		}
+		return idx;
+	}
+	public boolean remove(String name) {
+		int idx= findIndex(name);
+		if(idx!=-1) {
+//			String tmpName[]= new String[foodN.length-1];
+//			int tmpCost[]= new int[foodC.length-1];
+			Food food2[]= new Food[food.length-1];
+			for(int i=0,j=0;i<food.length;i++) {
+				if(idx!=i) {
+					food2[j]=food[i];
+//					tmpName[j]=this.foodN[i];
+//					tmpCost[j]=this.foodC[i];
+					j++;
+				}
+			}
+//			this.foodN=tmpName;
+//			this.foodC=tmpCost;
+			food=food2; 
+			
+			return true;
+		}
+		return false;
+	}
+	
 }
