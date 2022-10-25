3f8e6e880ec647a4d24a76575e1be00ee4412820
brown46 <rlawlsxo623@naver.com>
2022-10-25 15:08:05 +0900

    Worker클래스를 추상클래스로 바꾸기

--- java_demo07/src/kr/co/exam03/Worker.java
+++ java_demo07/src/kr/co/exam03/Worker.java
@@ -1,6 +1,6 @@
 package kr.co.exam03;
 
-public class Worker {
+public abstract class Worker {
 
 	private String name;
 	private int age;
@@ -17,18 +17,9 @@ public class Worker {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public void goWork() {
-		System.out.println("출근합니다");
-	}
-	public void offWork() {
-		System.out.println("퇴근합니다");
-	}
-	
-	public void lunch() {
-		System.out.println("점심을 먹습니다");
-	}
-	public void overtime() {
-		System.out.println("야근을 합니다");
-	}
+	public abstract void goWork() ;
+	public abstract void offWork(); 
+	public abstract void lunch(); 
+	public abstract void overtime(); 
 	
 }
