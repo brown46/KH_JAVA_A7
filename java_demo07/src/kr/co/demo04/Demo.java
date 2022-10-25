244a6e2c03303be04353bd1bc65626f2c38688be
brown46 <rlawlsxo623@naver.com>
2022-10-25 14:58:02 +0900

    추상 클래스

---
+++ java_demo07/src/kr/co/demo04/Demo.java
@@ -0,0 +1,31 @@
+package kr.co.demo04;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		/*
+		 * 추상(abstract) 클래스
+		 * 		- 미완성 클래스로 클래스 안의 메서드가 전부 구현되어 있지는 않다.
+		 * 		- 메서드 선언부만 있고 구현은 하지 않은 추상 메서드를 가지는 클래스
+		 * 		- abstract 키워드로 만들어서 사용
+		 * 		- 추상 클래스로는 직접 객체를 생성할 수 없다.
+		 * 		  (단 참조 타입으로는 사용가능 -> 다형성으로 사용 가능)
+		 * 		- 추상 클래스를 상속하는 일반 클래스는 미완성 메서드를 반드시 구현해야 한다.
+		 * 
+		 * 추상 클래스를 사용할 때의 이점
+		 *		- 추상 클래스를 상속하는 자식 클래스들의 메서드가 일관성 있는 이름으로 사용가능
+		 *		- 반드시 필요한 기능에 대해서는 강제하여 사용할 수 있다. 
+		 */
+
+		
+		Person p1= new Student();
+		p1.setName("홍길동");
+		p1.setAge(23);
+		p1.setGender('M');
+		
+		System.out.println(p1);
+		System.out.println(p1.getFirstName());
+		System.out.println(p1.getLastName());
+	}
+
+}
