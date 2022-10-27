d3d96d7de14cfde8ab32beee7c5c1be78139ea80
brown46 <rlawlsxo623@naver.com>
2022-10-27 15:17:53 +0900

    String 관련 클래스 연습문제

--- java_demo08/src/kr/co/exam02/Exam.java
+++ java_demo08/src/kr/co/exam02/Exam.java
@@ -22,20 +22,85 @@ public class Exam {
 		Scanner sc = new Scanner(System.in);
 		
 	
-		String arr[]= new String[10];
-		while(true) {
+		String arr[]= new String[0];
+loop2:	while(true) {
+			System.out.println("전화번호를 입력해 주세요");
 			String input =sc.nextLine();
 			arr= input.split("-");
-		
-			if(arr[0].equals("010")) {
+//			if(arr[0].equals("010")==false) {
+//				System.out.println("010으로 시작해야 합니다.");
+//				continue;
+//			}
+			if(input.startsWith("010")==false) {
+				System.out.println("010으로 시작해야 합니다.");
 				continue;
 			}
-			System.out.println(arr[0]);
-			String.join("-", arr[0],arr[1],"****");
-			System.out.println(Arrays.toString(arr));
+			if(arr.length!=3) {
+				System.out.println("하이픈으로 구분해주세요");
+				continue;
+			}
+			
+			if(input.length()!=13) {
+				System.out.println("하이픈을 포함한13자리 번호를 입력해야 합니다.");
+				continue;
+			}
+			for(int i=1; i<arr.length;i++) {
+				for(int j=0;j<arr[i].length();j++) {
+					if(arr[i].length()!=4) {
+						System.out.println("하이픈을 잘못 입력하셨습니다.");
+						continue loop2;
+					}
+				}
+			}
+//			for(int i=0; i<arr[1].length();i++) {
+//				if(arr[1].charAt(i)>'9'||arr[1].charAt(i)<'0') {
+//					System.out.println("숫자를 입력주세요");
+//					continue loop2;
+//				}
+//			}
+//			for(int i=0; i<arr[2].length();i++) {
+//				if(arr[2].charAt(i)>'9'||arr[2].charAt(i)<'0') {
+//					System.out.println("숫자를 입력주세요");
+//					continue loop2;
+//				}
+//			}
+			for(int i=1; i<arr.length;i++) {
+				for(int j=0;j<arr[i].length();j++) {
+					if(arr[i].charAt(j)>'9'||arr[i].charAt(j)<'0') {
+						System.out.println("숫자를 입력해주세요");
+						continue loop2;
+					}
+				}
+			}
+			String arr1=String.join("-", arr[0],arr[1],"****");
+			System.out.println(arr1);
 			break;
 			
 		}
+	loop1:	while(true) {
+			System.out.println("주민등록번호를 입력해주세요");
+			String input2= sc.nextLine();
+			if(input2.length()!=13) {
+				System.out.println("하이픈을 포함한 13자리를 입력해주세요");
+				continue;
+			}
+			
+			for(int i=0; i<13;i++) {
+				if(input2.charAt(i)>'9'||input2.charAt(i)<'0') {
+					System.out.println("숫자를 입력주세요");
+					continue loop1;
+				}
+			}
+			
+			if(input2.charAt(6)=='1'||input2.charAt(6)=='3') {
+				System.out.println("남자입니다.");
+				break;
+			}else if(input2.charAt(6)=='2'||input2.charAt(6)=='4') {
+				System.out.println("여자입니다.");
+				break;
+			}else System.out.println("잘못 입력하셨습니다");
+		}
+		
 	}
 
 }
