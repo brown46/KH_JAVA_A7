5daa6e5cdfc57591e4b67561ea9f29fe936e1c8a
brown46 <rlawlsxo623@naver.com>
2022-10-28 15:16:47 +0900

    에외처리 연습문제

---
+++ java_demo09/src/kr/co/demo03/user/Member.java
@@ -0,0 +1,28 @@
+package kr.co.demo03.user;
+
+import kr.co.demo03.user.exception.UserException;
+
+public class Member {
+
+	private String name;
+
+	public Member() {
+	}
+
+	public Member(String name) {
+		try {
+			setName(name);
+		} catch (UserException e) {
+			this.name="";
+		}
+		
+	}
+
+	public void setName(String name) throws UserException {
+		if (name != null) {
+			this.name = name;
+		}else {
+			throw new UserException("잘못된 사용자 이름");
+		}
+	}
+}
