45c32588a1eca9fe03870c3c4475b1f8bd927c52
brown46 <rlawlsxo623@naver.com>
2023-01-10 15:09:27 +0900

    MVC모델 적용한 방명록 페이지

---
+++ dynamic_web01/src/main/java/controller/VisitController.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dto.VisitDTO;
+import model.service.VisitService;
+
+/**
+ * 방명록을 위한 컨트롤러
+ * @author user1
+ *
+ */
+public class VisitController extends HttpServlet{
+	
+	/**
+	 * 방명록 페이지 요청이 있을 경우 사용하는 메서드 
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		VisitService service =new VisitService();
+		List<VisitDTO> visitList= service.getList();
+//		System.out.println(visitList);
+		req.setAttribute("dataList", visitList);
+		req.getRequestDispatcher("/WEB-INF/view/visit.jsp").forward(req, resp);
+	}//링크 클릭
+	
+	/**
+	 * 방명록 작성 후 저장 요청이 있을 경우 사용하는 메서드
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		VisitDTO data =new VisitDTO();
+		data.setContext(req.getParameter("context"));
+		data.setNickname(req.getParameter("nickname"));
+//		req.setCharacterEncoding("utf-8");
+		System.out.println(data);
+		VisitService service = new VisitService();
+		boolean result= service.add(data); //전달 후 응답 반환
+		if(result) {
+			resp.sendRedirect("./visit");
+		}else {
+			req.getRequestDispatcher("/WEB-INF/view/fail.jsp").forward(req, resp);
+		}
+		resp.getWriter().flush();
+		
+	}//<form method="post"
+}
---
+++ dynamic_web01/src/main/webapp/WEB-INF/view/visit.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.util.List, model.dto.VisitDTO, java.text.SimpleDateFormat" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>방명록</title>
+</head>
+<body>
+	<a href="./">메인</a>
+	<h2>방명록</h2>
+	<form action="./visit" method="post" >
+		<div>
+			<textarea rows="1" cols="25" name="context" placeholder="닉네임 입력"></textarea>
+		</div>
+		<div>
+			<textarea rows="3" cols="25" name="nickname" placeholder="내용 입력"></textarea>
+		</div>
+		<div>
+			<button type="submit">등록</button>
+		</div>
+	</form>
+	<ul>
+	<%SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY년 MM월 dd일 a hh시 mm분 ss초"); %>
+	<% for(VisitDTO data:(List<VisitDTO>)request.getAttribute("dataList")){ %>
+		<li><%= data.getNickname() %> | <%= data.getContext() %> | <%= dateFormat.format(data.getCreateDate()) %></li>
+	<% } %>
+	</ul>
+</body>
+</html>
