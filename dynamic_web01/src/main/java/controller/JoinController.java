062427797130fe2455916c035b4c7d32ed9b0295
brown46 <rlawlsxo623@naver.com>
2023-01-17 11:18:49 +0900

    회원가입 페이지

---
+++ dynamic_web01/src/main/java/controller/JoinController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dto.UserDTO;
+import model.service.UserService;
+
+public class JoinController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userId= req.getParameter("userId");
+		String password = req.getParameter("password");
+		String email =req.getParameter("email");
+		
+		UserDTO dto = new UserDTO();
+		dto.setUserId(userId);
+		dto.setPassword(password);
+		dto.setEmail(email);
+		
+		UserService service = new UserService();
+		int result = service.add(dto);
+		
+		switch (result) {
+		case 1:
+			resp.sendRedirect("./login");  //로그인 페이지로 이동
+			break;
+		case -1:
+			resp.sendRedirect("./error"); 
+			break;
+		case -2:
+			req.setAttribute("error", "동일한 아이디가 사용중입니다.");
+			
+//			resp.sendRedirect("./join");// redirect 를 사용하면 사용자 입력값이 사라진다.
+			req.getRequestDispatcher("/WEB-INF/view/join.jsp").forward(req, resp);
+			//입력했던값이 삭제되지 않음
+		}
+		
+
+	}
+}
---
+++ dynamic_web01/src/main/java/model/service/UserService.java
@@ -0,0 +1,26 @@
+package model.service;
+
+import model.dao.UserDAO;
+import model.dto.UserDTO;
+
+public class UserService {
+
+	public int add(UserDTO dto) {
+		UserDAO dao = new UserDAO();
+		
+		
+		UserDTO data=dao.selectUser(dto);//데이터가 없으면 null이 저장됨
+		
+		if(data ==null) {		
+			int count = dao.insert(dto);
+			if(count==1) {
+				dao.commit(); dao.close();
+				return 1;
+			}
+			dao.rollback(); dao.close();
+			return -1;
+		}
+		return -2;  //아이디가 중복일시
+	}
+	
+}
