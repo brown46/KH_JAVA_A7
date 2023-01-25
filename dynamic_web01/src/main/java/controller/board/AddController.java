0202733d75b37c6645b0bb9ea08aa7ae5d8f5be9
brown46 <rlawlsxo623@naver.com>
2023-01-25 11:52:03 +0900

    게시판 작업 중

---
+++ dynamic_web01/src/main/java/controller/board/AddController.java
@@ -0,0 +1,10 @@
+package controller.board;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+//@WebServlet("/board/add")
+public class AddController extends HttpServlet{
+
+	
+}
---
+++ dynamic_web01/src/main/java/controller/board/IndexController.java
@@ -0,0 +1,10 @@
+package controller.board;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+//@WebServlet("/board")
+public class IndexController extends HttpServlet{
+
+	
+}
